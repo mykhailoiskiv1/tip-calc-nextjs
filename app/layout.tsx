760d40e6342c3ea6ca1cc000c924dfc9432109c5
Misha376 <iskivmisha@gmail.com>
2024-10-03 12:40:11 +0100

    fix icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="../public/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico"/>
       </head>
       <body>
         <TipProvider>
